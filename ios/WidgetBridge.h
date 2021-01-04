e331ed4be6178ed6a39820dd56a9d097542a1b01
luatnd <abc.nguyenluat@gmail.com>
2021-01-04 18:20:17 +0700

    Add h file to compile to Swift code

---
+++ ios/WidgetBridge.h
@@ -0,0 +1,22 @@
+//
+//  WidgetBridge.h
+//  WidgetBridge
+//
+//  Created by Luat on 31/12/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+#import <React/RCTBridgeModule.h>
+//#import <React/RCTEventEmitter.h>
+
+#ifndef WidgetBridge_h
+#define WidgetBridge_h
+
+// --- If you use Swift, you don't need header anymore
+// @interface Pushdy : UIResponder <UIApplicationDelegate, RCTBridgeModule>
+//@interface WidgetBridgeInterface : RCTEventEmitter <RCTBridgeModule>
+//+(void)doSthFoo:(NSString*)msg;
+//+(void)initWithContext:(NSString*)clientKey
+//               delegate:(UIApplication *)delegate didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
+//@end
+
+#endif /* WidgetBridge_h */
